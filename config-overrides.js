c6669ed04dbf7ddfa4a29a8a451b0f1f533fc83a
csm <c_sm@ngarihealth.com>
2020-04-15 17:20:25 +0800

    内网穿透

--- config-overrides.js
+++ config-overrides.js
@@ -1,38 +1,24 @@
-const { override, fixBabelImports, addLessLoader, addWebpackAlias } = require('customize-cra');
+const { override, fixBabelImports, addLessLoader, addWebpackAlias, overrideDevServer } = require('customize-cra');
 const theme = require('./antd-theme');
 const path = require("path");
 
-module.exports = override(
-    fixBabelImports('import', {
-        libraryName: 'antd-mobile',
-        style: true,
+module.exports = {
+    webpack: override(
+        // usual webpack plugin
+        fixBabelImports('import', {
+            libraryName: 'antd-mobile',
+            style: true,
+        }),
+        addLessLoader({
+            javascriptEnabled: true,
+            modifyVars: theme
+        }),
+        addWebpackAlias({
+            ["@"]: path.resolve(__dirname, "src")
+        }),
+    ),
+    devServer: overrideDevServer((config) => {
+        config.disableHostCheck = true
+        return config
     }),
-    addLessLoader({
-        javascriptEnabled: true,
-        modifyVars: theme
-    }),
-    addWebpackAlias({
-        ["@"]: path.resolve(__dirname, "src")
-    }),
-    
-    // (config)=>{ //暴露webpack的配置 config ,evn
-    //   // 去掉打包生产map 文件
-    //   // config.devtool = config.mode === 'development' ? 'cheap-module-source-map' : false;
-    //   if(process.env.NODE_ENV==="production") config.devtool=false;
-    //   if(process.env.NODE_ENV!=="development") config.plugins = [...config.plugins,...myPlugin]
-    //   //1.修改、添加loader 配置 :
-    //   // 所有的loaders规则是在config.module.rules(数组)的第二项
-    //   // 即：config.module.rules[2].oneof  (如果不是，具体可以打印 一下是第几项目)
-    //   // 修改 sass 配置 ，规则 loader 在第五项(具体看配置)
-    //   const loaders = config.module.rules.find(rule => Array.isArray(rule.oneOf)).oneOf;
-    //   loaders[5].use.push({
-    //     // test: '/\.(png|jpe?g|gif|svg|mp4(\?.*)?$/)',
-    //     loader: 'url-loader',
-    //     options: {
-    //         esModule: false
-    //     }
-    // })
-    //   return config
-    // }
-   
-  );
+};
--- src/views/login/index.js
+++ src/views/login/index.js
@@ -28,6 +28,7 @@ class Login extends Component {
         <Cancel onClick={this.cancelLogin}></Cancel>
         <img src={require('@/statics/img/title.png')} style={logo} alt="头像" ></img>
         <Slogan>唯有美食与爱不可辜负</Slogan>
+        <a href="weixin://">打开微信</a>
         {/* <a href = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxf5fff2aa6e0b1af7&redirect_uri=https%3A%2F%2F08b2ee24.ngrok.io%2F&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect'>直接跳转到微信提供的网页</a> */}
         <Button style={wechatLogin} onClick={this.handleWechatBtnClick}>使用微信登录</Button>
         <Button style={phoneLogin} onClick={this.handlePhoneBtnClick}>手机登录注册</Button>
@@ -38,19 +39,39 @@ class Login extends Component {
   handleWechatBtnClick() {
     let return_uri = encodeURIComponent('http://127.0.0.1:3000/personInfo');  
     let url = ('https://open.weixin.qq.com/connect/oauth2/authorize?appid='+AppID+'&redirect_uri='+return_uri+'&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect');
+
+    // let url = 'weixin://dl/business/?ticket=ta428dhj739hg3efe6e';
     document.location = url;
+
   }
 
   handlePhoneBtnClick() {
     this.props.history.push('/phoneRegister');
   }
+  // https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxf5fff2aa6e0b1af7&redirect_uri=http%3A%2F%2F127.0.0.1%3A3000%2FpersonInfo&response_type=code&scope=snsapi_userinfo&state=STATE&connect_redirect=1#wechat_redirect
+
+  // https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxf5fff2aa6e0b1af7&redirect_uri=http%3A%2F%2F127.0.0.1%3A3000%2FpersonInfo&response_type=code&scope=snsapi_userinfo&state=STATE&connect_redirect=1&uin=MTY2NjcxNzI0&key=601e91c206e212893e333f08e66ecbba9ac656ff465c386b14ec4f82c8cb54e3c0a29bd8fc2e8a77c198f4c974e8bcb0&pass_ticket=G/oAtnGuAdcptJN/+VHIqLkLRcHGsCTxaRod7cxVpCn9oAvgEh0Cvf4memnzWxgQp0YAzsa9crgmlh5sVKv0Pw==
   
   cancelLogin() {
     this.props.history.push({
       pathname: '/tab/home/recommend'
     })
   }
-
+  //判断是否微信浏览器
+  isWeixinBrowser() {  
+    var ua = navigator.userAgent.toLowerCase();  
+    var result = (/micromessenger/.test(ua)) ? true : false;
+    if (result) {
+        console.log('你正在访问微信浏览器');
+    }
+    else {
+        console.log('你访问的不是微信浏览器');
+    }
+    return result;
+  };
+  componentDidMount() {
+    this.isWeixinBrowser();
+  }
 }
 
 export default Login;
